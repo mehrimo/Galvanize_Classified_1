758cdc2e6c8f87104047d6556e02489047df8981
Mehri Russo <mehrimova@gmail.com>
2017-01-30 10:23:49 -0700

    GET tests passing

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -2,9 +2,36 @@
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+router.get('/', (req, res, next) => {
+  knex('classifieds')
+  .select('id', 'title', 'description', 'price', 'item_image')
+  .then((results) => {
+    res.send(200, results);
+  })
+  .catch ((err) => {
+    next(err);
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  let ad_id = req.params.id;
+
+  knex('classifieds')
+  .where({
+    id: ad_id
+  })
+  .select('id', 'title', 'description', 'price', 'item_image')
+  .then((results) => {
+    res.send(200, results[0]);
+  })
+  .catch ((err) => {
+    next(err);
+  });
+});
+
+
 
 module.exports = router;
